6dd70261f5aa7290c373c16df7325ffbba32882b
LFR3052 <luca.b.frei@outlook.com>
2022-01-03 10:55:08 +0100

    feat(angular): updated to angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { MenuGeneratorComponent } from './menu-generator/menu-generator.component';
-import { OverviewComponent } from './overview/overview.component';
 import { MenuDetailComponent } from './menu-detail/menu-detail.component';
+import { MenuGeneratorComponent } from './menu-generator/menu-generator.component';
+import { MenuOverviewComponent } from './menu-overview/menu-overview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MenuDetailComponent,
     MenuGeneratorComponent,
-    OverviewComponent,
-    MenuDetailComponent
+    MenuOverviewComponent
   ],
   imports: [
     BrowserModule,

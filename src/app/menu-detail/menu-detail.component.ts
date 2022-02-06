3b133d2be93b807bed15109d80f9f53fe1bc85a5
Matthias Wattinger <matthias.wattinger@outlook.com>
2022-02-06 18:26:08 +0100

    refactor(menu-detail): format code

--- src/app/menu-detail/menu-detail.component.ts
+++ src/app/menu-detail/menu-detail.component.ts
@@ -6,20 +6,25 @@ import { MenuService } from '../shared/services/menu.service';
 @Component({
   selector: 'app-menu-detail',
   templateUrl: './menu-detail.component.html',
-  styleUrls: ['./menu-detail.component.scss']
+  styleUrls: ['./menu-detail.component.scss'],
 })
 export class MenuDetailComponent implements OnInit {
   menu: Menu = {} as Menu;
-  imageURL: string = "";
+  imageURL: string = '';
   ingredientsSplit: string[] = [];
 
-  constructor(private menuService: MenuService, private route: ActivatedRoute) { }
+  constructor(
+    private menuService: MenuService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.menuService.getMenuById(this.route.snapshot.paramMap.get('id')!).subscribe(res => {
-      this.menu = res
-      this.imageURL = `https://eatthis.fra1.digitaloceanspaces.com/${this.menu.image_id}.jpg`;
-      this.ingredientsSplit = res.ingredients.split("\n");
-    })  }
-
+    this.menuService
+      .getMenuById(this.route.snapshot.paramMap.get('id')!)
+      .subscribe((res) => {
+        this.menu = res;
+        this.imageURL = `https://eatthis.fra1.digitaloceanspaces.com/${this.menu.image_id}.jpg`;
+        this.ingredientsSplit = res.ingredients.split('\n');
+      });
+  }
 }
